9b881b72c03c04daace87ebb35954fcdb1824f78
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-02-23 13:26:43 -0500

    Added an assignment.js file for practive projects through this Udemy course

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "United States of America";
+let continent = "North America";
+let population = "579 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
