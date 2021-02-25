3a4975355e17dcb32f3e2f19630e50b55226f305
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-02-25 15:38:49 -0500

    Completed Data Types project

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -2,6 +2,14 @@ let country = "United States of America";
 let continent = "North America";
 let population = "579 million";
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language
+console.log(isIsland);
 console.log(population);
+console.log(country);
+console.log(language);
+
