612ef5c3be6c77796ba844a38cf543100521797f
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-04-05 21:53:23 -0400

    Completed Coding Challenge #1 project

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,15 +1,29 @@
-let country = "United States of America";
-let continent = "North America";
-let population = "579 million";
+// let country = "United States of America";
+// let continent = "North America";
+// let population = "579 million";
 
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
-let isIsland = false;
-let language
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+// let isIsland = false;
+// let language
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+
+// BMI calculation = 
+// BMI = mass / height ** 2 = mass / (height * height)
+
+let markMass = 78;
+let johnMass = 95;
+let markHeight = 1.69;
+let johnHeight = 1.88;
+
+console.log ((markMass ** 2) / markHeight);
+
+
+
+
 
